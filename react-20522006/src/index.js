377d09a35164a2a98dc28d65ec3270a173d1b338
Danthie <114122786+Danthiee@users.noreply.github.com>
2023-11-21 21:29:38 -0600

    szdxfg

--- react-20522006/src/index.js
+++ react-20522006/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MyButton from './MyButton';
 import MySection from './MySection';
-
+import MyComponent from '../component/1';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <MySection>
     <MyButton>My Butoon Text </MyButton>
+    <MyComponent/>
   </MySection>
 );
